fae1d9c6c38c11d18262656ef0551866bcbde672
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-08-19 18:27:29 +0100

    added global variables

---
+++ dist/index.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//declare global variables 
+var numberButtons = document.querySelectorAll(".number");
+var operationButtons = document.querySelectorAll(".operation");
+var equalsButton = document.querySelector(".equals");
+var clearButton = document.querySelector(".clear");
---
+++ index.js
@@ -0,0 +1,7 @@
+//declare global variables 
+
+const numberButtons = document.querySelectorAll(".number")
+const operationButtons = document.querySelectorAll(".operation")
+const equalsButton = document.querySelector(".equals")
+const clearButton = document.querySelector(".clear")
+
