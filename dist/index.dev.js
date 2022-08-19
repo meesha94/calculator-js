942ce95ac7c419ec4462708805b964f1ca7a694e
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-08-19 18:35:45 +0100

    iterate through number buttons

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -5,3 +5,18 @@ var numberButtons = document.querySelectorAll(".number");
 var operationButtons = document.querySelectorAll(".operation");
 var equalsButton = document.querySelector(".equals");
 var clearButton = document.querySelector(".clear");
+var answer = "";
+var input = "";
+
+var _loop = function _loop(index) {
+  numberButtons[index].addEventListener("click", function () {
+    input += numberButtons[index].innerText;
+    document.querySelector(".input").value = input;
+  });
+};
+
+for (var index = 0; index < numberButtons.length; index++) {
+  _loop(index);
+}
+
+;
--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const operationButtons = document.querySelectorAll(".operation")
 const equalsButton = document.querySelector(".equals")
 const clearButton = document.querySelector(".clear")
 
+let answer = ""
+let input = ""
+
+for (let index=0; index<numberButtons.length; index++) {
+    numberButtons[index].addEventListener("click",  () => {
+        input += numberButtons[index].innerText;
+        document.querySelector(".input").value = input;
+    });
+};
+
