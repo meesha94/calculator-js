d9732bf9e811633221111eb8b3b93f34c445538e
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-08-19 19:29:32 +0100

    functionalit for each operation

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -5,8 +5,9 @@ var numberButtons = document.querySelectorAll(".number");
 var operationButtons = document.querySelectorAll(".operation");
 var equalsButton = document.querySelector(".equals");
 var clearButton = document.querySelector(".clear");
+var operators = ("+", "-", "*", "/");
 var answer = "";
-var input = "";
+var input = ""; //iterate through number buttons and return the specific numnber
 
 var _loop = function _loop(index) {
   numberButtons[index].addEventListener("click", function () {
@@ -19,12 +20,50 @@ for (var index = 0; index < numberButtons.length; index++) {
   _loop(index);
 }
 
-;
+; // iterare through operation keys, turn them into numbers from strings and what sum to do with each operation
 
 var _loop2 = function _loop2(_index) {
   operationButtons[_index].addEventListener("click", function () {
-    input += operationButtons[_index].innerText;
-    document.querySelector(".input").value = input;
+    if (input.includes("+")) {
+      var mathsArray = input.split("+");
+
+      var equation = function equation(previousValue, currentValue) {
+        return parseFloat(previousValue) + parseFloat(currentValue);
+      };
+
+      input = mathsArray.reduce(equation);
+      input += operationButtons[_index].innerText;
+    } else if (input.includes("-")) {
+      var _mathsArray = input.split("-");
+
+      var _equation = function _equation(previousValue, currentValue) {
+        return parseFloat(previousValue) - parseFloat(currentValue);
+      };
+
+      input = _mathsArray.reduce(_equation);
+      input += operationButtons[_index].innerText;
+    } else if (input.includes("*")) {
+      var _mathsArray2 = input.split("*");
+
+      var _equation2 = function _equation2(previousValue, currentValue) {
+        return parseFloat(previousValue) * parseFloat(currentValue);
+      };
+
+      input = _mathsArray2.reduce(_equation2);
+      input += operationButtons[_index].innerText;
+    } else if (input.includes("/")) {
+      var _mathsArray3 = input.split("/");
+
+      var _equation3 = function _equation3(previousValue, currentValue) {
+        return parseFloat(previousValue) / parseFloat(currentValue);
+      };
+
+      input = _mathsArray3.reduce(_equation3);
+      input += operationButtons[_index].innerText;
+    } else {
+      input += operationButtons[_index].innerText;
+      document.querySelector(".input").value = input;
+    }
   });
 };
 
--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const numberButtons = document.querySelectorAll(".number")
 const operationButtons = document.querySelectorAll(".operation")
 const equalsButton = document.querySelector(".equals")
 const clearButton = document.querySelector(".clear")
+const operators = ("+", "-", "*", "/")
 
-let answer = ""
-let input = ""
+let answer = "";
+let input = "";
+
+//iterate through number buttons and return the specific numnber
 
 for (let index=0; index<numberButtons.length; index++) {
     numberButtons[index].addEventListener("click",  () => {
@@ -15,9 +18,44 @@ for (let index=0; index<numberButtons.length; index++) {
     });
 };
 
+
+// iterare through operation keys, turn them into numbers from strings and what sum to do with each operation
 for (let index = 0; index<operationButtons.length; index++) {
     operationButtons[index].addEventListener("click", () => {
+        if(input.includes("+")){
+            const mathsArray = input.split("+");
+            const equation = (previousValue, currentValue) =>
+            parseFloat(previousValue) + parseFloat(currentValue);
+            input = mathsArray.reduce (equation);
+            input += operationButtons[index].innerText
+        } else if (input.includes("-")){
+            const mathsArray = input.split("-");
+            const equation = (previousValue, currentValue) => 
+            parseFloat(previousValue) - parseFloat(currentValue);
+            input = mathsArray.reduce (equation);
+            input += operationButtons[index].innerText
+        } else if (input.includes("*")){
+            const mathsArray = input.split("*");
+            const equation = (previousValue, currentValue) =>
+            parseFloat(previousValue) * parseFloat(currentValue);
+            input = mathsArray.reduce (equation);
+            input += operationButtons[index].innerText
+        } else if (input.includes("/")){
+            const mathsArray = input.split("/");
+            const equation = (previousValue, currentValue) =>
+            parseFloat(previousValue) / parseFloat(currentValue);
+            input = mathsArray.reduce (equation);
+            input += operationButtons[index].innerText
+        } else {
         input += operationButtons[index].innerText;
         document.querySelector(".input").value = input;
+        }
     })
+
+    
+
+
+
+
+
 }
